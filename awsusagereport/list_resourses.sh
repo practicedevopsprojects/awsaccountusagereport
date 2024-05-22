6e7f7ca2d83d03eb6a7c128dee4bb452962ab001
Anushakalari <141445193+Anushakalari@users.noreply.github.com>
2024-05-22 14:47:11 +0530

    Update list_resourses.sh

--- awsusagereport/list_resourses.sh
+++ awsusagereport/list_resourses.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+set -x
+# get the instances in account
+aws ec2 describe-instances
+# get the only instances ids in account
+aws ec2 describe-instances | jq ' .Reservations[].Instances[].InstanceId'
+aws s3 ls
+aws lambda list-functions
+aws iam list-users
 
